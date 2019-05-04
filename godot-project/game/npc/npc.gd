6a8298bf8b8ac85922208be5502868d8265ae150
Moisés J. Bonilla Caraballo (moisesjbc) <moisesjbc@gmail.com>
2019-05-04 14:39:17 +0100

    Added drag and drop to NPCs

--- godot-project/game/npc/npc.gd
+++ godot-project/game/npc/npc.gd
@@ -4,11 +4,13 @@ extends KinematicBody2D
 export (int) var speed = 250
 var player = null
 var alive = true
+var dragging : bool = false
 
 
 func _ready():
 	player = get_node("../player")
 	add_to_group("NPCs")
+	set_process_input(true)
 
 
 func decide_direction(delta):
@@ -23,7 +25,7 @@ func look_at_player():
 	Look at the player's position
 	"""
 	look_at(player.global_position)
-	
+
 
 func kill():
 	alive = false
@@ -34,3 +36,17 @@ func _process(delta):
 	if alive:
 		move_and_collide(decide_direction(delta))
 		look_at_player()
+	else:
+		if dragging:
+			global_position = get_global_mouse_position()
+
+
+func _on_area_input_event(viewport, event, shape_idx):
+	"""
+	If NPC is dead, handle drag and drop
+	"""
+	if not alive:
+		if event is InputEventMouseButton and \
+			event.pressed and \
+			event.button_index == BUTTON_LEFT:
+				dragging = not dragging
