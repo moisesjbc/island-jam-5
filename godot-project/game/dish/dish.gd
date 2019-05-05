7544b31479d291a2eefff1aecc5e7d02da883c98
Moisés J. Bonilla Caraballo (moisesjbc) <moisesjbc@gmail.com>
2019-05-05 15:20:26 +0100

    Added dish-related sounds

--- godot-project/game/dish/dish.gd
+++ godot-project/game/dish/dish.gd
@@ -11,9 +11,11 @@ func set_combined_effect(effect_1, effect_2):
 		effect_2 = aux
 	effect = "effect_" + effect_1 + '_' + effect_2
 	$sprite.play(effect_1 + '_' + effect_2)
+	$respawn_sound.play()
 
 
 func _on_dish_body_entered(body):
 	queue_free()
 	if body.name == 'player':
 		get_parent().process_effect(effect, 5)
+		body.get_node('eat_sound').play()
--- godot-project/menus/button/button.gd
+++ godot-project/menus/button/button.gd
@@ -4,3 +4,7 @@ export (String) var label = ''
 
 func _ready():
 	$label.text = label
+
+
+func _on_button_mouse_entered():
+	$hover_sound.play()
